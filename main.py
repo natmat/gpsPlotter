f79177e173a782e533b058859ff18aa1bc2d66a9
nathan <nmattick@gmail.com>
2020-10-31 08:19:47 +0000

    Adding the main and log files

---
+++ main.py
@@ -0,0 +1,66 @@
+from sys import exit
+import tkinter as tk
+from tkinter import filedialog
+import random
+
+# Popup to select input station_data.sql file
+root = tk.Tk()
+root.withdraw()
+log_file_name = "asdo.log"
+# log_file_name = filedialog.askopenfilename()
+# if not log_file_name:
+#     print("Error: must select an asdo.log file")
+#     exit(1)
+print("Reading data from {}".format(log_file_name))
+
+# Set up the DataFrame for the oolumns to extract from the station_data.sql waypoint table data
+import pandas as pd
+df = pd.DataFrame(columns=['waypoint_name', 'waypoint_type', 'waypoint_id', 'waypoint_lat', 'waypoint_long', 'waypoint_radius', 'tap_tsi_code'])
+
+# Create a basemap centred somewhere around East Anglia
+import folium
+# gps_map = folium.Map(location=[51.648611, -0.052778], zoom_start=10, tiles="CartoDB dark_matter")
+gps_map = folium.Map(location=[51.648611, -0.052778], zoom_start=10)
+
+# Open station_data.sql file for parsing
+# data_file = "/Users/Nathan/PycharmProjects/stationplotter/stadler_station_data.sql"
+import re
+log_file = open(log_file_name, 'r')
+for line in log_file:
+    # Regex for the DataFrame columns
+
+    # 2020/10/28 16:01:52.270464120 NavMan: 420.Car-A is DISCONNECTED at ( GPS+Odo, 52.4192, 0.745005, 0, 0.141 )
+
+    gps_line_re = re.compile(".*NavMan.*\((?P<confidence>.*?),(?P<lat>[^,]+),(?P<lon>[^,]+)")
+    match = gps_line_re.search(line)
+    if match:
+        gps_d = match.groupdict()
+        print (gps_d)
+
+        geofence = 10000
+        circle_colours = ['#00ae53', '#86dc76', '#daf8aa', '#ffe6a4', '#ff9a61', '#ee0028']
+        icon_colours = ['darkpurple', 'lightred', 'gray', 'black', 'blue', 'orange', 'beige',
+                        'cadetblue', 'darkred', 'red', 'lightblue', 'white', 'purple',
+                        'lightgreen', 'darkblue', 'lightgray', 'darkgreen', 'green', 'pink']
+
+        # Add markers to the map
+        lat = gps_d["lat"]
+        lon=  gps_d["lon"]
+        confidence = gps_d["confidence"]
+
+        if confidence.lower() == 'gps':
+            icon_colour = 'green'
+        folium.Marker(location=[lat, lon],
+                      popup = gps_d,
+                      icon=folium.Icon(color=random.choice(icon_colours), icon='cloud')).\
+            add_to(gps_map)
+
+
+        folium.Circle([lat, lon], radius=geofence, fill=True,
+                      fill_color=random.choice(circle_colours), fill_opacity=0.25, tooltip=confidence).\
+            add_to(gps_map)
+
+map_file = log_file_name + ".map.html"
+print("Writing to file {}".format(map_file))
+gps_map.save(map_file)
+
