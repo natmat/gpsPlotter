7a203cb6cae7824fc727a6768e0ab37a8d0fbfff
nathan <nmattick@gmail.com>
2020-10-31 09:16:16 +0000

    First pass with the gen6parser

--- main.py
+++ main.py
@@ -32,12 +32,14 @@ for line in log_file:
     # 2020/10/28 16:01:52.270464120 NavMan: 420.Car-A is DISCONNECTED at ( GPS+Odo, 52.4192, 0.745005, 0, 0.141 )
 
     gps_line_re = re.compile(".*NavMan.*\((?P<confidence>.*?),(?P<lat>[^,]+),(?P<lon>[^,]+)")
+    # gps_line_re = re.compile(".*Publishing NavigationMessage.*\((?P<confidence>.*?),(?P<lat>[^,]+),(?P<lon>[^,]+)")
+
     match = gps_line_re.search(line)
     if match:
         gps_d = match.groupdict()
         print (gps_d)
 
-        geofence = 10000
+        RADIUS = 10000
         circle_colours = ['#00ae53', '#86dc76', '#daf8aa', '#ffe6a4', '#ff9a61', '#ee0028']
         icon_colours = ['darkpurple', 'lightred', 'gray', 'black', 'blue', 'orange', 'beige',
                         'cadetblue', 'darkred', 'red', 'lightblue', 'white', 'purple',
@@ -46,18 +48,30 @@ for line in log_file:
         # Add markers to the map
         lat = gps_d["lat"]
         lon=  gps_d["lon"]
-        confidence = gps_d["confidence"]
+        confidence = gps_d["confidence"].lower().strip()
 
-        if confidence.lower() == 'gps':
+        if confidence == 'gps':
             icon_colour = 'green'
+        elif confidence == 'gps+odo':
+            icon_colour = 'orange'
+        elif confidence == 'odo':
+            icon_colour = 'red'
+        else:
+            print ("Can't match confidence: ", confidence.lower())
+            continue
+
+
         folium.Marker(location=[lat, lon],
                       popup = gps_d,
-                      icon=folium.Icon(color=random.choice(icon_colours), icon='cloud')).\
+                      icon=folium.Icon(icon_colour, icon='cloud')).\
             add_to(gps_map)
 
 
-        folium.Circle([lat, lon], radius=geofence, fill=True,
-                      fill_color=random.choice(circle_colours), fill_opacity=0.25, tooltip=confidence).\
+        folium.Circle([lat, lon],
+                      radius=RADIUS, fill=True,
+                      fill_color=random.choice(circle_colours),
+                      fill_opacity=0.25,
+                      tooltip=confidence).\
             add_to(gps_map)
 
 map_file = log_file_name + ".map.html"
