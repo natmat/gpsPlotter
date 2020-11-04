bf6451fd4f1e1db741b4c1e611d6e1ab2693764a
nathan <nmattick@gmail.com>
2020-11-04 07:03:56 +0000

    Updated the Header to use more simple parsing

--- Header.py
+++ Header.py
@@ -1,16 +1,34 @@
-class Header:
-  def __init__(self, d):
-    self.date = [] if d is None else d["date"]
-    self.hours = [] if d is None else d["hours"]
-    self.minutes = [] if d is None else d["minutes"]
-    self.seconds = [] if d is None else d["seconds"]
-    self.ip_addr = [] if d is None else d["ip_addr"]
-    self.service = [] if d is None else d["service"]
 
-    # self.date = [] if date is None else date
-    # self.hours = [] if hours is None else hours
-    # self.minutes = [] if minutes is None else minutes
-    # self.seconds = [] if seconds is None else seconds
-    # self.ip_addr = [] if ip_addr is None else ip_addr
-    # self.service = [] if service is None else service
+class Header():
+  def __init__(self):
+    self.header = None
+
+  def stripHeader(self, line):
+    split_line = line.split()
+    self.date = split_line[0]
+    self.hours = split_line[1].split(":")[0]
+    self.minutes = split_line[1].split(":")[1]
+    self.ip_addr = split_line[2]
+    self.adeiv = split_line[3]
+    self.service = split_line[4]
+
+  def get_hours(self):
+    return(self.hours)
+
+def main():
+  test_line = " 2020/10/29 10:05:00.038423342 10.177.156.21 AUD Navigation Navigation.cpp@325: Publishing NavigationMessage( 0, 0, 0, ERROR, 0 )"
+  h = Header()
+  h.stripHeader(test_line)
+
+  #Use list comprehension to remove the header from line
+  line_header = ' '.join([str(elem) for elem in test_line.split()[:5]])
+  line_without_header = ' '.join([str(elem) for elem in test_line.split()[5:]])
+  print(line_header)
+  print(line_without_header)
+  
+  return(line_without_header)
+
+if __name__ == "__main__":
+  # execute only if run as a script
+  main()
 
